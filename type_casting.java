d4c36c89774723801b384f5f6b5380ef3c42f825
jahnavi1505 <jahnavimulukutla@gmail.com>
2025-10-01 13:53:28 +0530

    Add files via upload

---
+++ type_casting.java
@@ -0,0 +1,20 @@
+package tns_backend_training.Basic_Programs;
+
+public class type_casting {
+    public static void main(String[] args) {
+    //implicit type casting
+    short x1 = 10;
+    int y1 = x1;
+    System.out.println("Implicit Type Casting");
+    System.out.println(y1);
+
+    int x2 = 1204123;
+    short y2 = (short)x2;
+    System.out.println("Explicit Type Casting");
+    System.out.println(y2);
+
+
+
+    
+}
+}
